aabff54e189f18468010b47e129a8761e67301e2
Sabid Ansari <ansarisabid84@gmail.com>
2024-01-26 13:02:11 +0530

    designed header & footer. Also created new home component for content of the body

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+    },
+    {
+        path: 'home', loadComponent: () => import("./dashboard/home/home.component").then((c) => c.HomeComponent)
+    },
+    {
+        path: '**', redirectTo: 'home'
+    }
+];
--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faGoogle, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  fa_icons = {
+    "faGithub": faGithub,
+    "faLinkedIn": faLinkedin,
+    "faGoogle": faGoogle,
+    "faLightbulb": faLightbulb
+  }
 
 }
